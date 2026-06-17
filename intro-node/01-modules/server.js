602f9530caba61528c9adb8f6f7d834f056921fc
jalvarenga11 <juan.alvarenga@ess.hn>
2026-06-16 18:47:25 -0600

    manejo de middlewares y params

--- intro-node/01-modules/server.js
+++ intro-node/01-modules/server.js
@@ -2,8 +2,15 @@ import express from "express"
 
 // escuha peticiones y emitir respuestas
 const server = express()
+
+//quitar el encabezado en la respuesta
+server.disable('x-powered-by')
+
 //definir que puerto es el que será escuchado
 const PORT = 3000
+
+//middlewares
+server.use(express.json()) // interceptar los paquetes de datos y transformarlos a formato json, ademas de integrarlo al body de la petición
 
 // definicion de rutas
 server.get('/products', (request, response) => {
@@ -14,29 +21,41 @@ server.post('/products', (request, response) => {
 
     console.log(request.url)
     console.log(request.method)
-    console.log(request.body)
-    console.log(request.kqwjhsenfgdvlj)
+
+    console.log(request.body) // {}
+
     console.log(request.query)
     console.log(request.headers)
-
-
 
     response.status(201).json({
         "message": "producto creado"
     })
 })
 
-server.put('/products', (request, response) => {
-    response.send('guardar un productso nuevo')
+server.put('/products/:id', (request, response) => {
+
+    console.log(request.params)
+    const { id } = request.params
+
+    response.json({
+        message: `Producto id ${id} actualziado`
+    })
 })
 
-server.delete('/product', (request, response) => {
+server.delete('/product/:id', (request, response) => {
     response.send('guardar un producto nuevo')
 })
 
 
 server.get('/', (request, response) => {
     response.send('<h1>Hola mundo desde Express Js</h1>')
+})
+
+// ruta por default
+server.use((request, response) => {
+    response.status(404).json({
+        message: `La ruta \'${request.path}\' no existe`
+    })
 })
 
 
